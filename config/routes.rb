fd2220d90c20ebbe2089f7362df26e041019dd06
Sonal Singh <sonal@codearena.in>
2018-10-18 15:06:40 +0530

    removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :dashboard, only: :index
-  resources :books
-  resources :categories
+  resources :books, only: :show
+  resources :categories, only: :show
   get 'dashboard/search'
   get 'dashboard/category_list'
   root to: 'dashboard#index'
